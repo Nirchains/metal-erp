683217a2ca49021ed0b1056ef1d50da8a11fba83
Nirchains <nirchains@gmail.com>
2017-04-11 15:04:49 +0200

    fixtures

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -28,7 +28,8 @@ after_install = "erpnext.setup.install.after_install"
 
 boot_session = "erpnext.startup.boot.boot_session"
 notification_config = "erpnext.startup.notifications.get_notification_config"
-get_help_messages = "erpnext.utilities.activation.get_help_messages"
+#PFG
+#get_help_messages = "erpnext.utilities.activation.get_help_messages"
 
 on_session_creation = "erpnext.shopping_cart.utils.set_cart_count"
 on_logout = "erpnext.shopping_cart.utils.clear_cart_count"
